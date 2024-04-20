254844af500a42c1af1be7e92712172a77c98b20
Gustavo Gião <122322049+imgugaaz@users.noreply.github.com>
2024-04-20 17:23:22 +0100

    Métodos Getters e Setters (Status)

--- ProjetoSwingLp/src/Status.java
+++ ProjetoSwingLp/src/Status.java
@@ -1,4 +1,23 @@
 public class Status {
-    int Id;
-    String State;
+
+    private int id;
+    private String state;
+
+    // Métodos de acesso para os atributos privados
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
 }
